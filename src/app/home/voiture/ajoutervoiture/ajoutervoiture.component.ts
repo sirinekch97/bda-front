f17d94e101e1c2204430facffecac1cbaadee9c5
eyaballoum <balloumeya@gmail.com>
2020-01-26 09:36:10 -0800

    first commit

--- src/app/home/voiture/ajoutervoiture/ajoutervoiture.component.ts
+++ src/app/home/voiture/ajoutervoiture/ajoutervoiture.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { Service } from 'src/app/service';
 
 @Component({
   selector: 'app-ajoutervoiture',
@@ -8,9 +9,39 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class AjoutervoitureComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: Service) { }
 
+  registerForm = new FormGroup({
+    nom: new FormControl(''),
+    marque: new FormControl(''),
+    Modele: new FormControl(''),
+    price: new FormControl(''),
+    numChassis: new FormControl(''),
+    puissanceFiscale: new FormControl(''),
+    nbCylindre: new FormControl(''),
+    datepicker1: new FormControl('')
+  });
+
+  id;
   ngOnInit() {
+    this.service.getLastId().subscribe(data =>{
+      this.id=data;
+    })
+  }
+  onSubmit(){
+    var voiture = {id: this.id,
+               numImmatriculion: this.registerForm.value["Numimmat"],
+               marque: this.registerForm.value["marque"],
+               modele: this.registerForm.value["modele"],
+               prixLoc: this.registerForm.value["price"],
+               numChassis: this.registerForm.value["numChassis"],
+               puissanceFiscale: this.registerForm.value["puissanceFiscale"],
+               nbCylindre: this.registerForm.value["nbCylindre"],
+               datepicker1: this.registerForm.value["datepicker1"]}
+    this.service.ajouterVoiture(voiture).subscribe(data =>{
+      
+    })
+    console.log(this.registerForm.value);
   }
 
 }
